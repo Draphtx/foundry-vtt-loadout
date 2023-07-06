82c37b8d1047cbe156fb2abf3c638a30099d26c1
Brandon Marlow <draphtx@gmail.com>
2023-07-06 16:25:55 -0400

    Added 0 level to equip preferences for no reason except that's where the index should start

--- cpr2.0/setLoadouttile.js
+++ cpr2.0/setLoadouttile.js
@@ -11,6 +11,7 @@ const loadoutTileDialog = new Dialog({
         <label for="weightSelect">Preference Weight</label>
         <select name="weightSelect">
           <option value=1>1</option>
+          <option value=1>1</option>
           <option value=2>2</option>
           <option value=3>3</option>
           <option value=4>4</option>
