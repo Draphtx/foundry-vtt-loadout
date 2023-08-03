c7c45c637ec687ad97eb169f44552ad08bd3a1a8
Draphtx <Draphtx@users.noreply.github.com>
2023-08-03 17:38:11 -0400

    Tidied settings menu

--- scripts/config.js
+++ scripts/config.js
@@ -1,15 +1,5 @@
 Hooks.once("init", function () {
-
-    // Display itemToken names on hover
-    game.settings.register("loadouts", "loadouts-token-names", {
-        name: "Show Token Nameplates",
-        hint: "Display the linked item's name as the token's nameplate",
-        scope: "world",
-        config: true,
-        default: true,
-        type: Boolean
-    });
-
+    
     game.settings.register("loadouts", "loadouts-managed-item-types", {
         name: "Managed Item Types",
         hint: "A comma-separated list of item types to be managed by Loadouts",
@@ -19,7 +9,7 @@ Hooks.once("init", function () {
         type: String
     });
 
-    // Magazine counters
+    /* WAITING ON CONFIG SUPPORT INITIATIVE
     game.settings.register("loadouts", "loadouts-allow-unconfigured-items", {
         name: "Allow Unconfigured Items",
         hint: "Allows players to add unconfigured items to their inventories",
@@ -28,6 +18,7 @@ Hooks.once("init", function () {
         default: true,
         type: Boolean
     });
+    */
 
     game.settings.register("loadouts", "loadouts-managed-actor-types", {
         name: "Managed Actor Types",
@@ -38,7 +29,7 @@ Hooks.once("init", function () {
         type: String
     });
 
-    // Magazine counters
+    /* WAITING ON CONFIG SUPPORT INITIATIVE, ALSO DEPRECATED IN NEAR FUTURE
     game.settings.register("loadouts", "loadouts-magazine-bars", {
         name: "Enable Magazine Bars",
         hint: "Where applicable, reflect magazine count using the itemToken's health bar",
@@ -47,7 +38,9 @@ Hooks.once("init", function () {
         default: true,
         type: Boolean
     });
+    */
 
+    /* WAITING ON CONFIG SUPPORT INITIATIVE
     game.settings.register("loadouts", "loadouts-teleport-to-stash", {
         name: "Enable Remote Stashing",
         hint: "When a 'carried' slot cannot be found, the player may choose to 'teleport' the item back to an uncarried stash",
@@ -56,13 +49,26 @@ Hooks.once("init", function () {
         default: false,
         type: Boolean
     });
+    */
 
+    /* WAITING ON CONFIG SUPPORT INITIATIVE
     game.settings.register("loadouts", "loadouts-full-add-anyway", {
         name: "Enable Add When Full",
         hint: "When no slots are available, the player may choose to add the item to their inventory anyway",
         scope: "world",
         config: true,
         default: false,
+        type: Boolean
+    });
+    */
+
+    // Display itemToken names on hover
+    game.settings.register("loadouts", "loadouts-token-names", {
+        name: "Show Token Nameplates",
+        hint: "Display the linked item's name as the token's nameplate",
+        scope: "world",
+        config: true,
+        default: true,
         type: Boolean
     });
 
