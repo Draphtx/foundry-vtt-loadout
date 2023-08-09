a07506c244126fa2f4e664e7d64d54eb2c786cb8
Draphtx <Draphtx@users.noreply.github.com>
2023-08-09 16:25:06 -0400

    Fixed positioning error, somehow

--- scripts/loadoutsHooks.js
+++ scripts/loadoutsHooks.js
@@ -124,22 +124,19 @@ function findValidTiles(itemDocument, itemOrientation){
     // Return the valid tiles, sorted by preference weight
     return validTiles.sort((a, b) => a.flags.loadouts.weight < b.flags.loadouts.weight ? -1 : 1);
 }
-/*
+
 // Find the available positions (if any) for the item in each tile
 function processTilePositions(itemDocument, validTiles, itemOrientation){
-    let tilePositions = [];
-    let selectedTile = null
-    let isStacked = false
+    var tilePositions = [];
+    var selectedTile = null
     for(const loadoutsTile of validTiles){
-
-        // Find any tokens that are already within the Tile's bounds
-        let existingTokens = loadoutsTile.parent.tokens.filter(
-            t => t.x >= loadoutsTile.x <= (loadoutsTile.x + loadoutsTile.width) && 
-                 t.y >= loadoutsTile.y <=(loadoutsTile.y + loadoutsTile.height))
-
         // If the item has a stack setting, check to see whether any of the existing tokens are stacks of the same item with room to spare
         // By doing this here, we ensure that the tile's weight is still accounted for when looking for existing stacks
         if(itemDocument.flags.loadouts.stack.max > 1){
+            // Find any tokens that are already within the Tile's bounds
+            let existingTokens = loadoutsTile.parent.tokens.filter(
+                t => t.x >= loadoutsTile.x <= (loadoutsTile.x + loadoutsTile.width) && 
+                     t.y >= loadoutsTile.y <=(loadoutsTile.y + loadoutsTile.height))
             let validStacks = existingTokens.filter(t => t.name == itemDocument.name && 
                                                         (t.flags?.loadouts?.stack?.members?.length + 1) <= (itemDocument?.flags?.loadouts?.stack?.max)
             // Let's not account for individual item quantities for now - I'm not sure if or when we'll do anything with that
@@ -152,79 +149,7 @@ function processTilePositions(itemDocument, validTiles, itemOrientation){
             }
         }
 
-        let tilePositions = getTilePositions(loadoutsTile, itemOrientation.size_x, itemOrientation.size_y)
-        if(! tilePositions.length){
-            if(itemOrientation.size_x != itemOrientation.size_y){
-                tilePositions = getTilePositions(loadoutsTile, itemOrientation.size_y, itemOrientation.size_x)
-                if(tilePositions.length){
-                    itemOrientation.rotation = 90
-                }
-            }
-        }
-        if(tilePositions.length){
-            selectedTile = loadoutsTile;
-            break;
-        }
-    };
-
-    // Get an array of possible positions for the item to land if nothing was blocking its space
-    function getTilePositions(loadoutsTile, itemSizeL, itemSizeH){
-        // TODO: need a way to 'reserve' certain slots at the tile configuration level, such that the whole slot is used (preferably)
-        //// Currently we are covering for this by highly-prioritizing single-item slots, but that's just smoke & mirrors
-        let itemPositions = []
-        console.log(`getting positions for tile ${loadoutsTile.id}`)
-        for(let rowNum of Array(loadoutsTile.height/loadoutsTile.parent.grid.size).keys()){
-            for(let colNum of Array(loadoutsTile.width/loadoutsTile.parent.grid.size).keys()){
-                let tilePosition = {
-                    "x1": loadoutsTile.x + (colNum * loadoutsTile.parent.grid.size), "y1": loadoutsTile.y + (rowNum * loadoutsTile.parent.grid.size), 
-                    "x2": loadoutsTile.x + (colNum * loadoutsTile.parent.grid.size) + (itemSizeL * loadoutsTile.parent.grid.size), "y2": loadoutsTile.y + (rowNum * loadoutsTile.parent.grid.size) + (itemSizeH * loadoutsTile.parent.grid.size),
-                }
-                if((tilePosition.x1 + (itemSizeL * loadoutsTile.parent.grid.size) <= loadoutsTile.x + loadoutsTile.width) && (tilePosition.y1 + (itemSizeH * loadoutsTile.parent.grid.size) <= loadoutsTile.y + loadoutsTile.height)){
-                    itemPositions.push(tilePosition)
-                } else {
-                    console.log("Discarded position")
-                }
-            }
-        }
-
-        
-        console.log(tilePositions)
-        
-        // Find any tokens that may already be over the tile's area
-        let blockingTokens = loadoutsTile.parent.tokens.filter(
-            t => t.x >= loadoutsTile.x <= (loadoutsTile.x + loadoutsTile.width) && 
-                 t.y >= loadoutsTile.y <=(loadoutsTile.y + loadoutsTile.height))
-
-        // Here there be dragons. One liner that filters the potential token creation positions with the spaces blocked by existing tokens.
-        // There is something going on here with the use of the itemSize * gridSize that makes me have to do this extra step of determining 
-        // which filter to use...this should be refactorable to a single filter but my brain is refusing to deal with it right now.
-        for(let blockingToken of blockingTokens){
-            // If the blockingToken is >= the new item, the item should use the filter but with Math.max
-            if(blockingToken.width >= itemSizeL || blockingToken.height > itemSizeH){
-                itemPositions = itemPositions.filter(p => 
-                    p.x1 >= Math.max(blockingToken.x + blockingToken.width * loadoutsTile.parent.grid.size, blockingToken.x + itemSizeL * loadoutsTile.parent.grid.size) || blockingToken.x >= p.x2 || 
-                    p.y1 >= Math.max(blockingToken.y + blockingToken.height * loadoutsTile.parent.grid.size, blockingToken.y + itemSizeH * loadoutsTile.parent.grid.size) || blockingToken.y >= p.y2
-                    )
-            // If the blockingToken is < the new item, the item should use the filter but with Math.min
-            } else {
-                itemPositions = itemPositions.filter(p => 
-                    p.x1 >= Math.min(blockingToken.x + blockingToken.width * loadoutsTile.parent.grid.size, blockingToken.x + itemSizeL * loadoutsTile.parent.grid.size) || blockingToken.x >= p.x2 || 
-                    p.y1 >= Math.min(blockingToken.y + blockingToken.height * loadoutsTile.parent.grid.size, blockingToken.y + itemSizeH * loadoutsTile.parent.grid.size) || blockingToken.y >= p.y2
-                    )
-            }
-        }
-        return itemPositions;
-    }
-    return [isStacked, selectedTile, tilePositions]
-}
-*/
-
-function processTilePositions(itemDoucment, validTiles, itemOrientation){
-    var tilePositions = [];
-    var selectedTile = null
-    for(const loadoutsTile of validTiles){
         tilePositions = getTilePositions(loadoutsTile, itemOrientation.size_x, itemOrientation.size_y)
-
         if(! tilePositions.length){
             if(itemOrientation.size_x != itemOrientation.size_y){
                 tilePositions = getTilePositions(loadoutsTile, itemOrientation.size_y, itemOrientation.size_x)
